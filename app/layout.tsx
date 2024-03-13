f1e816ae2cdae81f0e6c3c27dc1c54a6b1872d23
Unegbu Kingsley <kingsobino@gmail.com>
2024-03-13 17:52:34 +0100

    add chat widget

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-no-repeat min-h-dvh`} >
+
+        <Script>
+          {
+            `(function (w, d, s, o, f, js, fjs) {
+            w['MyWidget'] = o; w[o] = w[o] || function () {(w[o].q = w[o].q || []).push(arguments)};
+          js = d.createElement(s), fjs = d.getElementsByTagName(s)[0];
+          js.id = o; js.src = f; js.async = 1; fjs.parentNode.insertBefore(js, fjs);
+    }(window, document, 'script', 'mw', 'https://res.cloudinary.com/garhia/raw/upload/v1/heedbase/heedbase-widget.js'));
+          mw('init', {botId: "08ca426357e14d67b33df4c4c506c502" });`
+          }
+        </Script>
         <main>
           {children}
         </main>
