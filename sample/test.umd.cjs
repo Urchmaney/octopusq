a58e4fe74de2b542892ff3beaf78f3543f2558fd
Unegbu Kingsley <kingsobino@gmail.com>
2025-03-31 16:24:38 +0100

    update

--- sample/test.umd.cjs
+++ sample/test.umd.cjs
@@ -1,1 +1,10 @@
-(function(e,t){typeof exports=="object"&&typeof module<"u"?module.exports=t(require("react/jsx-runtime"),require("react")):typeof define=="function"&&define.amd?define(["react/jsx-runtime","react"],t):(e=typeof globalThis<"u"?globalThis:e||self,e.myLib=t(e["react/jsx-runtime"],e.react))})(this,function(e,t){"use strict";function n(){const[c,r]=t.useState(0);return e.jsxs("div",{children:[e.jsx("h1",{children:"Vite + React"}),e.jsxs("div",{className:"card",children:[e.jsxs("button",{onClick:()=>r(s=>s+1),children:["count is ",c]}),e.jsxs("p",{children:["Edit ",e.jsx("code",{children:"src/App.tsx"})," and save to test HMR"]})]}),e.jsx("p",{className:"read-the-docs",children:"Click on the Vite and React logos to learn more"})]})}return n});
+(function(global, factory) {
+  typeof exports === "object" && typeof module !== "undefined" ? module.exports = factory(require("react")) : typeof define === "function" && define.amd ? define(["react"], factory) : (global = typeof globalThis !== "undefined" ? globalThis : global || self, global.myLib = factory(global.react));
+})(this, function(react) {
+  "use strict";
+  function App() {
+    const [count, setCount] = react.useState(0);
+    return /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("h1", null, "Vite + React"), /* @__PURE__ */ React.createElement("div", { className: "card" }, /* @__PURE__ */ React.createElement("button", { onClick: () => setCount((count2) => count2 + 1) }, "count is ", count), /* @__PURE__ */ React.createElement("p", null, "Edit ", /* @__PURE__ */ React.createElement("code", null, "src/App.tsx"), " and save to test HMR")), /* @__PURE__ */ React.createElement("p", { className: "read-the-docs" }, "Click on the Vite and React logos to learn more"));
+  }
+  return App;
+});
