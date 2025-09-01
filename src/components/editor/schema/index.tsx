568a6bd20e902a4785833521ea20c813662edd18
Unegbu Kingsley <kingsobino@gmail.com>
2025-09-01 10:38:21 +0100

    fix heading uneditable version showing

--- src/components/editor/schema/index.tsx
+++ src/components/editor/schema/index.tsx
@@ -1,6 +1,6 @@
 import { BlockNoteSchema, createBlockSpecFromStronglyTypedTiptapNode, defaultBlockSpecs, defaultProps } from "@blocknote/core";
 import { cementSpec, CementRulesSpec } from "./cement";
-import { DOMNode } from "@tiptap/core";
+import { DOMNode, NodeConfig, NodeViewRendererProps } from "@tiptap/core";
 import { DocumentEditor } from "../documentEditor";
 
 const HEADING_LEVELS = [1, 2, 3, 4, 5, 6] as const;
@@ -11,35 +11,59 @@ export const schema = (docEditor: DocumentEditor) => BlockNoteSchema.create({
     ...defaultBlockSpecs,
     cementRules: CementRulesSpec(docEditor),
     cement: cementSpec(docEditor),
-    ...(Object.fromEntries(blockContents.map(blockName => [`o_${blockName}`, createBlockSpecFromStronglyTypedTiptapNode(
-      defaultBlockSpecs[blockName].implementation.node.extend({
-        name: `o_${blockName}`,
+    ...(Object.fromEntries(blockContents.map(blockName => {
+      const name = `o_${blockName}`;
+      const extendedProps: Partial<NodeConfig<any, any>>  = {
+        name,
+
+        addOptions() {
+          return {
+            editor: docEditor.blocknoteEditor,
+            ...(blockName === "heading" && {level: [1, 2, 3]}),
+          }
+        },
         renderHTML(props) {
-          const result = defaultBlockSpecs.paragraph.implementation.node.config.renderHTML?.call({
-            name: blockName, storage: this.storage, parent: null, options: this.options, editor: this.editor
+          const result = defaultBlockSpecs[blockName].implementation.node.config.renderHTML?.call({
+            name, storage: this.storage, parent: null, options: this.options, editor: this.editor
           }, props) as { dom: DOMNode, contentDOM?: HTMLElement };
           if (result && result.dom) {
             (result.dom as HTMLElement).contentEditable = "false"
           }
           return result
         },
-        parseHTML() {
-          const editor = this.options.editor
-          console.log(editor, this.name)
-          return [];
-        },
+
         addKeyboardShortcuts() { return {} },
         addInputRules() { return [] },
-        addProseMirrorPlugins() { return [] }
-      }),
-      {
-        ...(blockName === "heading" ? {
-          level: { default: 1, values: HEADING_LEVELS },
-          isToggleable: { default: false },
-          ...defaultProps
-        } : defaultProps)
+        addProseMirrorPlugins() { return [] },
+        ...(defaultBlockSpecs[blockName].implementation.node.config.addNodeView && {
+          addNodeView() {
+            return (props: NodeViewRendererProps) => {
+              const result = defaultBlockSpecs[blockName].implementation.node.config.addNodeView!.call({
+                name, type: this.type, storage: this.storage, parent: null, options: this.options, editor: this.editor
+              });
+              const val = result(props);
+              if (val?.dom) {
+                (val.dom as HTMLElement).contentEditable = "false";
+                return val;
+              }
+              return val;
+            }
+          }
+        })
       }
-    )])))
+
+      return [`o_${blockName}`, createBlockSpecFromStronglyTypedTiptapNode(
+        defaultBlockSpecs[blockName].implementation.node.extend(extendedProps),
+        {
+          ...(blockName === "heading" ? {
+            level: { default: 1, values: HEADING_LEVELS },
+            isToggleable: { default: false },
+            ...defaultProps
+          } : defaultProps)
+        }
+      )]
+    })))
+
   },
 });
 
@@ -48,3 +72,4 @@ export const resultSchema = BlockNoteSchema.create({
     ...defaultBlockSpecs,
   }
 })
+
